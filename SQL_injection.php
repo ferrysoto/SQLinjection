fe0af734ae847fab5b14696878977c376ac8290d
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-11-25 16:04:50 +0100

    SQL Unprotected

--- SQL_injection.php
+++ SQL_injection.php
@@ -40,32 +40,32 @@
 //
 //
 //
-//   $mysql = new MySQL();
-//   $mysql->setHost('localhost');
-//   $mysql->setUser('root');
-//   $mysql->setPassword('');
-//   $connection = $mysql->getConnection();
-//   mysqli_select_db($connection, 'SQLInjection');
-//   // $insert = "insert into user (username, name, password) value ('".$user."','".$user."',sha2('".$pass."',512));";
-//   $insert = "insert into user (username, name, password) value ('".$user."','".$user."','".$shadow."');";
-//
-//   $insertQuery = mysqli_query($connection, $insert);
-//   if (!$insertQuery) {
-//     $message  = 'Consulta invàlida: ' . mysqli_error() . "\n";
-//     $message .= 'Consulta realitzada: ' . $consulta;
-//     die($message);
-//   }
-//
-//   mysqli_close($connection);
+  $mysql = new MySQL();
+  $mysql->setHost('localhost');
+  $mysql->setUser('root');
+  $mysql->setPassword('');
+  $connection = $mysql->getConnection();
+  mysqli_select_db($connection, 'SQLInjection');
+  // $insert = "insert into user (username, name, password) value ('".$user."','".$user."',sha2('".$pass."',512));";
+  $insert = "insert into user (username, name, password) value ('".$user."','".$user."','".$shadow."');";
+
+  $insertQuery = mysqli_query($connection, $insert);
+  if (!$insertQuery) {
+    $message  = 'Consulta invàlida: ' . mysqli_error() . "\n";
+    $message .= 'Consulta realitzada: ' . $consulta;
+    die($message);
+  }
+
+  mysqli_close($connection);
 
   // Evitar SQL Injection
-  $user = htmlspecialchars($_POST["user"]);
-  $pass = htmlspecialchars($_POST["passwd"]);
+  // $user = htmlspecialchars($_POST["user"]);
+  // $pass = htmlspecialchars($_POST["passwd"]);
 
   // Query para probar eficacia:
   // ferran';CREATE TABLE MyGuests ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(30) NOT NULL, email VARCHAR(50), reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ) ;
-  // $user = $_POST["user"];
-  // $pass = $_POST["passwd"];
+  $user = $_POST["user"];
+  $pass = $_POST["passwd"];
   $shadow = hash('sha512',$pass);
   try {
     $pdo = new PDO("mysql:host=localhost;dbname=SQLInjection", "root", "P@ssw0rd");
